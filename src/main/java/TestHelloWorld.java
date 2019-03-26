f78a6a750e94f96bb6bac7aa8e716eda137c0b00
admin <admin@mycompany.com>
2019-03-26 10:26:23 +0000

    git-svn-id: http://tnlt588.vermeg.com/svn/Test/trunk@65 a571f8f7-2d87-cf46-8793-44d10af3a257

--- src/main/java/TestHelloWorld.java
+++ src/main/java/TestHelloWorld.java
@@ -1,7 +1,7 @@
 public class TestHelloWorld {
 
     public static void main (String args[]){
-        System.out.println("hello world01,");
+        System.out.println("hello world01");
 		System.out.println("hi branche1.0");
 		System.out.println("hi branche2.0");
     }
