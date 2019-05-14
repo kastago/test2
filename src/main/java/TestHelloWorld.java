d10d23492e802038218ab4d4ec6576773aa40a83
admin <admin@mycompany.com>
2019-05-14 11:07:00 +0000

    PST-213524 : bla bla blaaa
    git-svn-id: http://tnlt588.vermeg.com/svn/Test/branches/test1.0@69 a571f8f7-2d87-cf46-8793-44d10af3a257

--- src/main/java/TestHelloWorld.java
+++ src/main/java/TestHelloWorld.java
@@ -2,6 +2,6 @@ public class TestHelloWorld {
 
     public static void main (String args[]){
         System.out.println("hello world01");
-		System.out.println("hi guyss");
+		System.out.println("hi guyssss");
     }
 }
